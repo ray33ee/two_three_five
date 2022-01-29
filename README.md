77f04ef4863dac8d498e23550f8f89c644b544db
ray33ee <30669752+ray33ee@users.noreply.github.com>
2022-01-29 22:40:09 +0000

    Version 0.1.2 - Breadth first search algorithm added
    Signed-off-by: ray33ee <30669752+ray33ee@users.noreply.github.com>

--- README.md
+++ README.md
@@ -9,9 +9,11 @@ Starting with the number 0 and only using operations subtract 2, multiply by 3 a
 
 A simple and O(log(n)) solution used here works by working backwards from the number `n`, making the number divisible by 3 then dividing by 3 until one of several base cases is achieved.
 
+Our base cases are 0, 1 or 2 since any other number would be modified then divided by 3. Instead of 1 and 2, we test the base case before the division, and test for 3 and 6. This is because we can create 3 and 6 in fewer steps than we can create 1 or 2 then multiply by 3. We also use 12 as a base case as it appeared often and is quicker to catch than to wait (12 -> 4 -> 6 which is 2 steps plus the number of steps required by 6)
+
 Since we work in reverse from `n`, we perform the inverse (add 2, subtract 5 and divide by 3). If the number is 1 (mod 3) then we add 2, and if the number is 2 (mod 3) then we subtract 5.
 
-# Example [5, 2, 3, 3, 5]
+# Example
 
 Take the number 32. Starting with 32, we subtract 5 (since 32 is 2 (mod 3)) giving 27. Since 27 is not a base case, we divide by 3 to get 9. 9 is divisible by 3, so we can divide by 3 to get 3. Since 3 is our base case, we stop there. Since a base case of 3 is made by (+5, -2) we have the operations
 
--- main.py
+++ main.py
@@ -15,31 +15,33 @@ def reduce(x):
 
     l = []
 
+    xs = [x]
+
     # Simple cases (0 - 11):
     if x == 0:
-        return [], -1
+        return [], xs
     elif x == 1:
-        return [5, 2, 2], -1
+        return [5, 2, 2], xs
     elif x == 2:
-        return [2, 3, 5, 5, 2], -1
+        return [2, 3, 5, 5, 2], xs
     elif x == 3:
-        return [5, 2], -1
+        return [5, 2], xs
     elif x == 4:
-        return [2, 3, 5, 5], -1
+        return [2, 3, 5, 5], xs
     elif x == 5:
-        return [5], -1
+        return [5], xs
     elif x == 6:
-        return [2, 2, 5, 5], -1
+        return [2, 2, 5, 5], xs
     elif x == 7:
-        return [2, 5, 3, 2], -1
+        return [2, 5, 3, 2], xs
     elif x == 8:
-        return [5, 5, 2], -1
+        return [5, 5, 2], xs
     elif x == 9:
-        return [5, 2, 3], -1
+        return [5, 2, 3], xs
     elif x == 10:
-        return [5, 5], -1
+        return [5, 5], xs
     elif x == 11:
-        return [5, 3, 2, 2], -1
+        return [5, 3, 2, 2], xs
 
     while True:
         m = x % 3
@@ -53,33 +55,41 @@ def reduce(x):
             x = x - 5
             l.insert(0, 5)
 
+        xs.append(x)
+
         # At this point x is divisible by 3
 
         # If x is either 0, 3, 6 or 12 we can stop early as these numbers are our base cases
         if x == 0:
-            return l, 0
+            return l, xs
         elif x == 3:
             t = [5, 2]
             t.extend(l)
-            return t, 3
+            return t, xs
         elif x == 6:
             t = [5, 5, 2, 2]
             t.extend(l)
-            return t, 6
+            return t, xs
         elif x == 12:
             t = [2, 5, 3, 2, 5]
             t.extend(l)
-            return t, 12
+            return t, xs
+        elif x == 39:
+            t = [5, 3, 2, 3]
+            t.extend(l)
+            return t, xs
 
         # Divide the number by 3
         x = x / 3
         l.insert(0, 3)
 
+        xs.append(x)
+
 
 # Take the list, and apply all the operations to generate a number
-def build(l: list):
+def build(lu: list):
     x = 0
-    for op in l:
+    for op in lu:
         if op == 2:
             x = x - 2
         elif op == 3:
@@ -92,10 +102,68 @@ def build(l: list):
     return x
 
 
-for i in range(10000):
+# One layer in a breadth first search
+def bfs(numb, d: dict):
+
+    copy = d.copy()
+
+    for x in copy:
+        if x - 2 != 0 and x - 2 > -10 and x - 2 not in d:
+            d[x - 2] = (x, 2)
+        if x - 2 == numb:
+            return d, True
+
+        if x * 3 != 0 and x * 3 not in d:
+            d[x * 3] = (x, 3)
+        if x * 3 == numb:
+            return d, True
+
+        if x + 5 != 0 and x + 5 not in d:
+            d[x + 5] = (x, 5)
+        if x + 5 == numb:
+            return d, True
+
+    return d, False
+
+
+# Keep iterating over layers in a breadth first search until we are finished
+def find(nu):
+    di = {0:0}
+    f = False
+    while not f:
+        di, f = bfs(nu, di)
+
+    return trace(di, nu)
+
+
+# Given a completed search map, retrace the steps used to generate the number
+def trace(trace_map, num):
+
+    x = num
+    ops = []
+    while x != 0:
+        e = trace_map[x]
+        x = e[0]
+        ops.insert(0, e[1])
+
+    return ops
+
+
+for i in range(10001):
     print(str(i))
     l, ind = reduce(i)
     print("    " + str(l.__len__()) + " " + str(l) + " " + str(ind))
     if build(l) != i:
         print("assertion failed " + str(l) + " " + str(i))
         exit()
+
+
+n = 1000000
+
+m = find(n)
+
+print(m)
+
+print(reduce(n))
+
+print(build(m))
